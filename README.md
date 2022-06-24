bdba9f2423a61bddf79779ba346efffc1d91ba1b
root <root@lejzer.localdomain>
2022-06-23 19:49:25 -0600

    Pick randomly

---
+++ README.md
@@ -0,0 +1,13 @@
+# Anime's list 	🙂
+
+Web scraping with curl `curl` command will get raw data. Throughout the one-liner it gets cleaner until it achieves the Anime's names.
+
+With this project I want to randomize those names and get one name randomly.
+
+### Tasks I want to get done with this little project
+
+
+- [x] Scrap names from a website 
+- [x] Use `sed`, `awk` and `cut` to clean data
+- [x] Use loops to get the number of pages that the website has
+- [ ] Pick one name randomly
---
+++ anime_lists.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+url2="https://www.listchallenges.com/anime-list-1000/"
+url="https://www.listchallenges.com/anime-pat-need-to-watch/"
+
+for ((i=1; i<=24; i++))
+do
+    # Unix command 
+    curl -s --request GET $(echo "$url/list/$i" $1) | grep "item-name" -A 3 | tr -d "<>=/" | sed 's/div//g' | sed 's/class"item-name"//g' | sed 's/-//g' | sed 's/class"itemcheckbox"//g' | sed '/^[[:space:]]*$/d' | awk -F\t "NF > 1" OSF="\t" | awk -v start=0 -v end=3 '{ for (i=start; i<=end;i++) printf("%s%s", $i,(i==end) ? "\n" : OFS) }' | cut -f5- | awk '{$1=""}1'
+
+done > anime_list.txt
+
+cat anime_list | sort --randon-sort | head -n 1
+
+#----------------Tests----------------------------------#
+#list_ext=$url
+
+#curl --request GET $webpages | grep "item-name" -A 3 | tr -d "<>=/" | sed 's/div//g' | sed 's/class"item-name"//g' | sed 's/-//g' | sed 's/class"itemcheckbox"//g' | sed '/^[[:space:]]*$/d' | awk -F\t "NF > 1" OSF="\t" | awk -v start=0 -v end=3 '{ for (i=start; i<=end;i++) printf("%s%s",
+ #$i,(i==end) ? "\n" : OFS) }' | cut -f5- | awk '{$1=""}1'
+#---------------END--------------------------------------#
+
